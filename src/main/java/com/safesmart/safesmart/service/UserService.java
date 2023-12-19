ee6c233f1ab5bd5b86531e5f3a827314757331ad
ShaliniEdara <edara.shalini@gmail.com>
2023-12-19 19:24:23 +0530

    update-syn changes

--- src/main/java/com/safesmart/safesmart/service/UserService.java
+++ src/main/java/com/safesmart/safesmart/service/UserService.java
@@ -125,6 +125,119 @@ public class UserService {
 		
 		userInfoRepository.save(userInfo);
 	}
+	
+//	public void add(UserInfoRequest userInfoRequest) {
+//
+//
+//
+//		Role role = roleRepository.findByName(userInfoRequest.getRole());
+//
+//
+//
+//		UserInfo userInfo = userInfoRepository.findByUsername(userInfoRequest.getUsername());
+//
+//		if (userInfo != null) {
+//
+//			throw CommonException.CreateException(CommonExceptionMessage.ALREADY_EXISTS, "Username");
+//
+//		}
+//
+//		UserInfo infoPassword = userInfoRepository
+//
+//				.findByPassword(passwordEncrypt.encodePassword(userInfoRequest.getPassword()));
+//
+//		if (infoPassword != null) {
+//
+//			throw CommonException.CreateException(CommonExceptionMessage.ALREADY_EXISTS, "Password");
+//
+//		}
+//
+//
+//
+//		userInfo = new UserInfo();
+//
+//		userInfo.setId(userInfoRequest.getId());
+//
+//		userInfo.setRole(role);
+//
+//		userInfo.setUsername(userInfoRequest.getUsername());
+//
+//		userInfo.setPassword(passwordEncrypt.encodePassword(userInfoRequest.getPassword()));
+//
+//		userInfo.setCreate_time(LocalDate.now());
+//
+//		userInfo.setActive(true);
+//
+//		userInfo.setFirstName(userInfoRequest.getFirstName());
+//
+//		userInfo.setLastName(userInfoRequest.getLastName());
+//
+//		userInfo.setEmail(userInfoRequest.getEmail());
+//
+//		userInfo.setMobile(userInfoRequest.getMobile());
+//
+//		userInfo.setPassLength(userInfoRequest.getPassLength());
+//		userInfo.setActionStatus(ActionStatus.Created);
+//
+//		UserInfo dbUserInfo=null;
+//
+//		if (userInfoRequest.getLoggedUserId() != null) {
+//
+//			Optional<UserInfo> optionalAdminUser = userInfoRepository.findById(userInfoRequest.getLoggedUserId());
+//
+//			if (optionalAdminUser.isPresent()) {
+//
+//				UserInfo dbUser = optionalAdminUser.get();
+//
+//				UserInfo remoteUserInfo = remote_UserInfoRepository.findByIdentifier(dbUser.getIdentifier());
+//
+//				 dbUserInfo = dataMigrationService.convertToModel(remoteUserInfo,true );
+//
+//				if (dbUserInfo == null) {
+//
+//					userInfo.setSync(false);
+//
+//				}
+//
+//
+//
+//				if (dbUser != null) {
+//
+//					if (dbUser.getStoreInfo() != null) {
+//
+//						userInfo.setStoreInfo(dbUser.getStoreInfo());
+//
+//					}
+//
+//				}
+//
+//			}
+//
+//		}
+//
+//
+//
+//		try {
+//
+//			dbUserInfo.setSync(true);
+//
+//			remote_UserInfoRepository.save(dbUserInfo);
+//
+//		} catch (Exception e) {
+//
+//			userInfo.setSync(false);
+//
+//		}
+//
+//
+//
+//		userInfoRepository.save(userInfo);
+//
+//
+//
+//	}
+
+
 
 
 	public List<UserInfoResponse> findAllUser() {
